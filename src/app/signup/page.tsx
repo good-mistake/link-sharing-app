7d08b4bbf1c4de6b281990e095cee134e752043f
good-mistake <devillmk88@gmail.com>
2025-02-03 19:27:11 +0330

    changed url

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -40,7 +40,7 @@ export default function Signup() {
       console.log("Sending request with data:", formData);
 
       const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/auth/signup`,
+        `${process.env.NEXT_PUBLIC_API_URL}/api/auth/signup`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
