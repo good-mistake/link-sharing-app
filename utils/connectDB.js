b91d3d7849e2fa604bef41d42778af0ac76dd756
good-mistake <devillmk88@gmail.com>
2025-02-03 21:10:34 +0330

    ty

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -3,10 +3,13 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   if (mongoose.connection.readyState >= 1) return;
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      "mongodb+srv://admin:lwqrOHulLo3c6XCE@link-sharing-app-cluste.adnhv.mongodb.net/?retryWrites=true&w=majority&appName=link-sharing-app-cluster",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log("MognoDB Connected!");
   } catch (e) {
     console.error("MongoDB connection error:", e);
