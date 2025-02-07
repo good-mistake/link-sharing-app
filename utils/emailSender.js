e79f7ffc5147944773aa582744341ea598928fe2
good-mistake <devillmk88@gmail.com>
2025-02-07 21:54:38 +0330

    cha

--- utils/emailSender.js
+++ utils/emailSender.js
@@ -5,13 +5,10 @@ const sendVerificationEmail = async (email, token) => {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: false,
+    secure: true,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
-    },
-    tls: {
-      ciphers: "SSLv3",
     },
   });
 
