ab461cdf037f9e09001f04da4ea62bf40738425d
good-mistake <devillmk88@gmail.com>
2025-03-03 22:07:00 +0330

    err

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -586,13 +586,11 @@ export default function Home() {
                       </label>
                     </div>
                   </div>
-                  <div className="flex justify-center text-red-600 bg-red-100 border border-red-400 px-4 py-2 rounded-md text-sm mt-2 animate-fadeIn">
-                    {(errorMessageProfile || errorMessageImg) && (
-                      <div className="flex justify-center text-red-600 bg-red-100 border border-red-400 px-4 py-2 rounded-md text-sm mt-2 animate-fadeIn">
-                        {errorMessageProfile || errorMessageImg}
-                      </div>
-                    )}
-                  </div>
+                  {(errorMessageProfile || errorMessageImg) && (
+                    <div className="flex justify-center text-red-600 bg-red-100 border border-red-400 px-4 py-2 rounded-md text-sm mt-2 animate-fadeIn">
+                      {errorMessageProfile || errorMessageImg}
+                    </div>
+                  )}
                   {successProfile && (
                     <motion.div
                       className="w-full bg-green-100 border border-green-400 text-green-700 px-4 py-2 rounded-lg flex items-center gap-2"
