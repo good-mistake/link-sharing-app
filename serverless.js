f7ffc06037b20f4c9be1d52f25064a1e23510aad
good-mistake <devillmk88@gmail.com>
2025-03-04 21:28:17 +0330

    Vercel

--- serverless.js
+++ serverless.js
@@ -1,12 +1,13 @@
 import { createRouter } from "next-connect";
 import cors from "cors";
 import authRoutes from "./api/auth/index.js";
+import uploadRoute from "./api/upload.js";
 import dotenv from "dotenv";
+
 dotenv.config();
 const router = createRouter();
 
 router.use(cors());
-
 router.use(authRoutes);
-
+router.use("/api/upload", uploadRoute);
 export default router.handler();
