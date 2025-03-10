5370942c1dc5f583ca583c43ca03190da41f0653
good-mistake <devillmk88@gmail.com>
2025-03-10 23:47:05 +0330

    r

--- pages/api/auth/checkUser.js
+++ pages/api/auth/checkUser.js
@@ -1,6 +1,5 @@
-import connectDB from "@/utils/db";
-import User from "@/models/User";
-
+import connectDB from "../../../utils/connectDB.js";
+import User from "../../../model/User";
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method Not Allowed" });
