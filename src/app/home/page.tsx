7d61e741d35c8e5289fc11c97ddbd00c9134cb97
good-mistake <devillmk88@gmail.com>
2025-03-13 02:07:14 +0330

    ads

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -90,7 +90,7 @@ export default function Home() {
         id: prevLinks.length + 1,
         url: "",
         platform: /*selectedPlatform ||*/ "",
-        color: selectedColor || "",
+        color: "",
       },
     ]);
   };
