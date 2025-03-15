6c64088d5de56099337dfb5cd6d28d50958ce314
good-mistake <devillmk88@gmail.com>
2025-03-16 01:13:05 +0330

    dqw

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -94,7 +94,7 @@ const Login = () => {
       </div>
 
       <form
-        className="w-full sm:m-0 md:h-auto lg:scale-100 sm:max-w-xl 
+        className="w-full max-sm:m-0 md:h-auto lg:scale-100 sm:max-w-xl 
         bg-white p-6 rounded-lg shadow-lg space-y-4"
         onSubmit={handleLogin}
       >
